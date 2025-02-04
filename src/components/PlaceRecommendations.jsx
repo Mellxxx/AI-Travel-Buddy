fa0efbbc30e07c6f58b59f62fb502d3a4ac6ab9b
Mellxxx <thomas.mueller14.01.2005@gmail.com>
2025-02-04 15:37:56 +0100

    Bugfix

--- src/components/PlaceRecommendations.jsx
+++ src/components/PlaceRecommendations.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import PlacesImages from "./PlacesImages";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 const PlaceRecommendations = ({ country }) => {
     const [places, setPlaces] = useState([]);
--- src/pages/DestinationRecco.jsx
+++ src/pages/DestinationRecco.jsx
@@ -11,7 +11,7 @@ import {
     TooltipProvider,
 } from "../components/ui/tooltip";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 const DestinationRecco = () => {
     const options = [
